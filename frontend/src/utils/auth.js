51c7f9443013f7c4263036c58c9d419dd2366ec1
cjnm <seejan.raj@live.com>
2024-06-13 13:21:14 +0545

    auth issue fixed

--- frontend/src/utils/auth.js
+++ frontend/src/utils/auth.js
@@ -1,13 +1,10 @@
 const buildAuthHeaders = () => {
     const localUser = localStorage.getItem('simpleblog-user');
     if (localUser) {
-        const { jwt, username, id, avatar_url } = JSON.parse(localUser);
+        const { jwt } = JSON.parse(localUser);
         return {
             headers: {
-                Authorization: `${jwt}`,
-                'X-User-Id': id,
-                'X-User-Username': username,
-                'X-User-Avatar': avatar_url
+                Authorization: jwt
             }
         }
     } else {
